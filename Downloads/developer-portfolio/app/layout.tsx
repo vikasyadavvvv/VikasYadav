d5bcf66a973c99765610ababc240e8737d0e6984
vikasyadavvvv <157904214+vikasyadavvvv@users.noreply.github.com>
2025-06-21 00:07:21 +0530

    lightmode update

--- Downloads/developer-portfolio/app/layout.tsx
+++ Downloads/developer-portfolio/app/layout.tsx
@@ -2,7 +2,6 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import ThreeBackground from "./components/three-background"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -21,7 +20,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={inter.className}>
-        <ThreeBackground/>
         {children}</body>
     </html>
   )
